d459aa7b739fa9b7a456039d4e6e7e1026b85883
amabugat <34631534+amabugat@users.noreply.github.com>
2017-12-25 17:52:21 -0800

    Added Formatting and scroll reveal
    Added the about page, and started formatting the home page

--- js/script.js
+++ js/script.js
@@ -5,3 +5,17 @@ function openNav() {
 function closeNav() {
    document.getElementById("mySidenav").style.width = "0";
 }
+
+window.sr = ScrollReveal();
+
+sr.reveal('.nav1', {
+    duration: 3000,
+    origin: 'bottom',
+    reset: false
+})
+
+sr.reveal('#name', {
+    duration: 3000,
+    origin: 'bottom',
+    reset: false
+})
