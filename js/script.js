67b0e6c63d6231f775f4fb8f64987368634641c6
amabugat <34631534+amabugat@users.noreply.github.com>
2018-01-02 03:49:20 -0800

    Coded the "Aspect" Page
    -Still in the works
    -Implemented a photo grid system, however I may change it

--- js/script.js
+++ js/script.js
@@ -16,6 +16,22 @@ function toggleSidebar() {
       $('#sidebar').removeClass('active');
       $('.overlay').fadeOut();
    });
+    
+    
+//    var acc = document.getElementsByClassName("accordion");
+//    var i;
+//
+//    for (i = 0; i < acc.length; i++) {
+//          acc[i].addEventListener("click", function() {
+//            this.classList.toggle("active");
+//            var panel = this.nextElementSibling;
+//            if (panel.style.maxHeight){
+//              panel.style.maxHeight = null;
+//            } else {
+//              panel.style.maxHeight = panel.scrollHeight + "px";
+//            } 
+//          });
+//    }
 }
 
 function openNav() {
@@ -45,4 +61,9 @@ sr.reveal('#name', {
     reset: false
 })
 
+sr.reveal('#aspect_row', {
+   duration: 1000,
+    origin: 'bottom',
+    reset: true
+})
 
