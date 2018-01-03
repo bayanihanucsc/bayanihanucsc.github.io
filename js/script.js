4c42151cef7e3e76cb6c52b1ec65ef0273d0378a
amabugat <34631534+amabugat@users.noreply.github.com>
2018-01-02 20:41:40 -0800

    Implemented New Aspect Style
    In the FAQ page, I tried implementing a new way to display the aspects. Idk which one I will use yet

--- js/script.js
+++ js/script.js
@@ -17,21 +17,11 @@ function toggleSidebar() {
       $('.overlay').fadeOut();
    });
     
-    
-//    var acc = document.getElementsByClassName("accordion");
-//    var i;
-//
-//    for (i = 0; i < acc.length; i++) {
-//          acc[i].addEventListener("click", function() {
-//            this.classList.toggle("active");
-//            var panel = this.nextElementSibling;
-//            if (panel.style.maxHeight){
-//              panel.style.maxHeight = null;
-//            } else {
-//              panel.style.maxHeight = panel.scrollHeight + "px";
-//            } 
-//          });
-//    }
+$("#hoverLinks").find("a").hover(function () {
+    $("#PortfolioInfo>*").hide()
+        .filter($(this).data("filter")).show()
+})
+
 }
 
 function openNav() {
@@ -66,4 +56,5 @@ sr.reveal('#aspect_row', {
     origin: 'bottom',
     reset: true
 })
+
 
