b5f3be6478c54fcad0fcf362d4ebb1707e0d82ad
amabugat <34631534+amabugat@users.noreply.github.com>
2018-01-11 21:56:48 -0800

    Changed the about page
    -added appearing nav bar
    -Implemented an about page that I may change the design of

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+//Function for parallax scrolling
+
+$(window).scroll(function(e) {
+  parallax();
+})
+
+function parallax() {
+  var scroll = $(window).scrollTop();
+  var screenHeight = $(window).height();
+
+  $('.parallax--bg').each(function() {
+    var offset = $(this).offset().top;
+    var distanceFromBottom = offset - scroll - screenHeight
+    
+    if (offset > screenHeight && offset) {
+      $(this).css('background-position', 'center ' + (( distanceFromBottom  ) * 0.5) +'px');
+    } else {
+      $(this).css('background-position', 'center ' + (( -scroll ) * 0.5) + 'px');
+    }
+  })
+    
+}
+
+//Function for sidebar
 function toggleSidebar() {
    $('#sidebarCollapse').on('click', function() {
       $('#sidebar').addClass('active');   // adds .active class to sidebar, which makes the sidebar appear
@@ -11,14 +35,57 @@ function toggleSidebar() {
   
 }
 
-// function for hovering links
+//Function for hovering links
 $("#hoverLinks").find("a").hover(function () {
     $("#PortfolioInfo>*").hide()
         .filter($(this).data("filter")).show()
 })
 
 
+//Function for About Page Nav Bar
 
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
+
+
+
+//Funciton to fade in when scrolling
 window.sr = ScrollReveal();
 
 sr.reveal('.nav1', {
@@ -38,5 +105,4 @@ sr.reveal('#aspect_row', {
     origin: 'bottom',
     reset: true
 })
-
 
