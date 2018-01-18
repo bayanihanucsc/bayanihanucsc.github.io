bd053229ec4212faa9959db61fb9ca2b4170caea
amabugat <34631534+amabugat@users.noreply.github.com>
2018-01-17 23:14:19 -0800

    About Page
    Changed the layout of the about page
    -Added a calendar (May take it out)
    -Added a picture gallery

--- js/script.js
+++ js/script.js
@@ -1,45 +1,62 @@
 //Function for parallax scrolling
-
 $(window).scroll(function(e) {
-  parallax();
+    parallax();
 })
 
 function parallax() {
-  var scroll = $(window).scrollTop();
-  var screenHeight = $(window).height();
+    var scroll = $(window).scrollTop();
+    var screenHeight = $(window).height();
 
-  $('.parallax--bg').each(function() {
-    var offset = $(this).offset().top;
-    var distanceFromBottom = offset - scroll - screenHeight
-    
-    if (offset > screenHeight && offset) {
-      $(this).css('background-position', 'center ' + (( distanceFromBottom  ) * 0.5) +'px');
-    } else {
-      $(this).css('background-position', 'center ' + (( -scroll ) * 0.5) + 'px');
-    }
-  })
-    
+    $('.parallax--bg').each(function() {
+        var offset = $(this).offset().top;
+        var distanceFromBottom = offset - scroll - screenHeight
+
+        if (offset > screenHeight && offset) {
+            $(this).css('background-position', 'center ' + ((distanceFromBottom) * 0.5) + 'px');
+        } else {
+            $(this).css('background-position', 'center ' + ((-scroll) * 0.5) + 'px');
+        }
+    })
+
 }
 
 //Function for sidebar
 function toggleSidebar() {
-   $('#sidebarCollapse').on('click', function() {
-      $('#sidebar').addClass('active');   // adds .active class to sidebar, which makes the sidebar appear
-      $('.overlay').fadeIn();  // fades in the hidden .overlay element 
-   });
+    $('#sidebarCollapse').on('click', function() {
+        $('#sidebar').addClass('active'); // adds .active class to sidebar, which makes the sidebar appear
+        $('.overlay').fadeIn(); // fades in the hidden .overlay element 
+    });
 
-   $('#closeSidebar, .overlay').on('click', function(){
-      $('#sidebar').removeClass('active');
-      $('.overlay').fadeOut();
-   });
-  
+    $('#closeSidebar, .overlay').on('click', function() {
+        $('#sidebar').removeClass('active');
+        $('.overlay').fadeOut();
+    });
+
 }
 
 //Function for hovering links
-$("#hoverLinks").find("a").hover(function () {
+$("#hoverLinks").find("a").hover(function() {
     $("#PortfolioInfo>*").hide()
         .filter($(this).data("filter")).show()
 })
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
 
 
 //Function for About Page Nav Bar
@@ -50,7 +67,7 @@ var lastScrollTop = 0;
 var delta = 5;
 var navbarHeight = $('header').outerHeight();
 
-$(window).scroll(function(event){
+$(window).scroll(function(event) {
     didScroll = true;
 });
 
@@ -63,29 +80,29 @@ setInterval(function() {
 
 function hasScrolled() {
     var st = $(this).scrollTop();
-    
+
     // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
+    if (Math.abs(lastScrollTop - st) <= delta)
         return;
-    
+
     // If they scrolled down and are past the navbar, add class .nav-up.
     // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
+    if (st > lastScrollTop && st > navbarHeight) {
         // Scroll Down
         $('header').removeClass('nav-down').addClass('nav-up');
     } else {
         // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
+        if (st + $(window).height() < $(document).height()) {
             $('header').removeClass('nav-up').addClass('nav-down');
         }
     }
-    
+
     lastScrollTop = st;
 }
 
 
 
-//Funciton to fade in when scrolling
+//Function to fade in when scrolling
 window.sr = ScrollReveal();
 
 sr.reveal('.nav1', {
@@ -101,8 +118,13 @@ sr.reveal('#name', {
 })
 
 sr.reveal('#aspect_row', {
-   duration: 1000,
+    duration: 1000,
     origin: 'bottom',
     reset: true
 })
 
+sr.reveal('.mission', {
+    duration: 3000,
+    origin: 'bottom',
+    reset: false
+})
